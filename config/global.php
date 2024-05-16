86606f6224cd4f8940d1ed2711eb8ed48be4c03f
Romer <romer141016@gmail.com>
2024-05-16 11:06:17 -0400

    Hola

--- config/global.php
+++ config/global.php
@@ -2,5 +2,9 @@
 
 
 define("RUTA_BASE",$_SERVER['DOCUMENT_ROOT']."/");
-define("HTTP_BASE","http://127.0.0.1/PollosCristian")
+define("HTTP_BASE","http://127.0.0.1/PollosCristian");
+
+
+define('SECRET_KEY','POLLOSCRISS.jksdfwonfeaSDGMSDM345Lgomsda435lkfnsdalfASD7MGSDL2345FJSADOGJsadf232');
+define('ALGORITHM','HS256');
 ;
