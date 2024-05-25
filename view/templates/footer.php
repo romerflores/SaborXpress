8295c8d6be578fa05f6b2ba763ec36212a5de9a1
Romer <romer141016@gmail.com>
2024-05-25 17:43:53 -0400

    Division Header y footer

---
+++ view/templates/footer.php
@@ -0,0 +1,21 @@
+    <!-- base:js -->
+    <script src="<?php echo URL_RESOURCES;?>celestialAdmin/template/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="<?php echo URL_RESOURCES;?>celestialAdmin/template/js/off-canvas.js"></script>
+    <script src="<?php echo URL_RESOURCES;?>celestialAdmin/template/js/hoverable-collapse.js"></script>
+    <script src="<?php echo URL_RESOURCES;?>celestialAdmin/template/js/template.js"></script>
+    <script src="<?php echo URL_RESOURCES;?>celestialAdmin/template/js/settings.js"></script>
+    <script src="<?php echo URL_RESOURCES;?>celestialAdmin/template/js/todolist.js"></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <script src="<?php echo URL_RESOURCES;?>celestialAdmin/template/vendors/progressbar.js/progressbar.min.js"></script>
+    <script src="<?php echo URL_RESOURCES;?>celestialAdmin/template/vendors/chart.js/Chart.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- Custom js for this page-->
+    <script src="<?php echo URL_RESOURCES;?>celestialAdmin/template/js/dashboard.js"></script>
+    <!-- End custom js for this page-->
+  </body>
+</html>
