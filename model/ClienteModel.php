8f2334339ce8eb3b1259beae27f7baedc6a8b21c
CamargoCris <camargoj67@gmail.com>
2024-05-31 10:18:47 -0400

    reporteClient

--- model/ClienteModel.php
+++ model/ClienteModel.php
@@ -19,7 +19,9 @@ class ClienteModel extends ModeloBasePDO
 
     public function findid($p_id_cliente)
     {
-        $sql="SELECT id_cliente, nombre FROM cliente WHERE id_cliente=:p_id_cliente";
+        $sql="SELECT `id_cliente`, `nombre` 
+        FROM `cliente` 
+        WHERE id_cliente = :p_id_cliente";
         $param= array();
         //tenemos el atributo como string ya que el id(carnet o nit) puede tener complemento
         array_push($param,[':p_id_cliente',$p_id_cliente,PDO::PARAM_STR]);
@@ -27,12 +29,13 @@ class ClienteModel extends ModeloBasePDO
     }
     public function findpaginateall($p_filtro, $p_limit, $p_offset)
     {
-        $sql = "SELECT id_cliente, nombre 
-        FROM cliente 
+        $sql = "SELECT `id_cliente`, `nombre` 
+        FROM `cliente` 
         WHERE upper(concat(IFNULL(id_cliente,''),IFNULL(nombre,''))) 
         like concat('%',upper(IFNULL(:p_filtro,'')),'%') 
         limit :p_limit
         offset :p_offset"; //limit es para la cantidad de registros que se mostrara, y el offset es para decir desde que numero empezara la consulta
+        
         $param = array();
         array_push($param, [':p_filtro', $p_filtro, PDO::PARAM_STR]);
         array_push($param, [':p_limit', $p_limit, PDO::PARAM_INT]);
@@ -42,7 +45,7 @@ class ClienteModel extends ModeloBasePDO
         $var = parent::gselect($sql, $param);
         //esto es para contar
         $sqlcount = "SELECT count(1) as cant
-        FROM cliente
+        FROM `cliente`
         WHERE  upper(concat(IFNULL(id_cliente,''),IFNULL(nombre,''))) 
         like concat('%',upper(IFNULL(:p_filtro,'')),'%')";
         $param = array();
@@ -55,7 +58,7 @@ class ClienteModel extends ModeloBasePDO
     //crear nuevo cliente
     public function insert($p_id_cliente,$p_nombre)
     {
-        $sql="INSERT INTO cliente(id_cliente, nombre) VALUES (:p_id_cliente,:p_nombre); ";
+        $sql="INSERT INTO `cliente`(`id_cliente`, `nombre`) VALUES (:p_id_cliente,:p_nombre); ";
         $param=array();
         array_push($param,[':p_id_cliente',$p_id_cliente,PDO::PARAM_STR]);
         array_push($param,[':p_nombre',$p_nombre,PDO::PARAM_STR]);
@@ -66,14 +69,15 @@ class ClienteModel extends ModeloBasePDO
     //para modificar un cliente
     public function update($p_id_cliente,$p_nombre)
     {
-
-        $sql="UPDATE cliente SET nombre=:p_nombre WHERE id_cliente=':p_id_cliente'";
+        $sql="UPDATE `cliente` SET 
+        `nombre`=:p_nombre 
+        WHERE id_cliente = :p_id_cliente";
         $param=array();
         array_push($param,[':p_id_cliente',$p_id_cliente,PDO::PARAM_STR]);
         array_push($param,[':p_nombre',$p_nombre,PDO::PARAM_STR]);
 
         return parent::gupdate($sql,$param);
     }
-
-    
 }
+
+?>
